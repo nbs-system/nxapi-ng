e75e5007fae2ed9dfbb4b5c8481191a5048c6747
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-05-23 15:47:36 +0200

    Add a missing file

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -12,10 +12,10 @@ class TestProcessing(TestCase):
                                                      'learning': '0', 'zone0': 'HEADERS', 'score0': '8',
                                                      'var_name0': 'user-agent', 'cscore0': '$UWA', 'id0': '42000227',
                                                      'total_processed': '472', 'vers': '0.52'}
-        #self.assertFalse(processing.check_whitelist(whitelist, nxlog))
+        self.assertFalse(processing.check_whitelist(whitelist, nxlog))
 
         whitelist = {'rx:': 'rx:^[\\da-z_]+$', 'negative': '', 's:': '$LOG_TEST:1', 'id:': '42000456', 'mz:':
-            ['$ARGS_VAR:id', '$BODY_VAR:id']}
+            ['$ARGS_VAR:id', '$BODY_VAR:id', '$HEADERS:user-agent']}
         nxlog = {'uri': '/phpMyAdmin-2.8.2/scripts/setup.php', 'block': '0',
                                                      'total_blocked': '204', 'ip': 'X.X.X.X', 'server': 'Y.Y.Y.Y',
                                                      'learning': '0', 'zone0': 'HEADERS', 'score0': '8',

de1d839ffffbc5d5f88a8f42a6ef47a69b6efe33
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-05-24 18:20:59 +0200

    Add some processing tests

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -37,3 +37,11 @@ class TestProcessing(TestCase):
                                                      'var_name0': 'user-agent', 'cscore0': '$UWA', 'id0': '10',
                                                      'total_processed': '472', 'vers': '0.52'}
         self.assertFalse(processing.check_whitelist(whitelist, nxlog))
+
+        whitelist = {'mz': ['$HEADERS_VAR_X:^user-agent$'], 'wl': [42000227, 10]}
+        nxlog = {'uri': '/phpMyAdmin-2.8.2/scripts/setup.php', 'block': '0',
+                 'total_blocked': '204', 'ip': 'X.X.X.X', 'server': 'Y.Y.Y.Y',
+                 'learning': '0', 'zone0': 'HEADERS', 'score0': '8',
+                 'var_name0': 'user-agent', 'cscore0': '$UWA', 'id0': '10',
+                 'total_processed': '472', 'vers': '0.52'}
+        self.assertTrue(processing.check_whitelist(whitelist, nxlog))

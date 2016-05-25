10076cc8ed8adafeb1dd625e8790e86dbf3d23a2
Julien (jvoisin) Voisin <jvo@nbs-system.com>
2016-05-25 11:17:34 +0200

    Even more coverage

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = map(str.strip, f.read().splitlines())
+
 setup(
     name='nxapi',
     packages=find_packages(),
-    version='0.1'
+    version='0.1',
+    install_requires=['python-pcre']
 )

0063f39b265df45192e69551f85c665e8c6d72dc
Manuel Sabban <manu@abban.eu>
2017-05-26 16:35:16 +0200

    New tools to help import data to an elasticsearch instance.

--- nxapi/nxlog.py
+++ nxapi/nxlog.py
@@ -1,3 +1,9 @@
+import time
+from datetime import datetime
+import logging
+import re
+import socket
+
 try:
     from urlparse import parse_qs
 except ImportError:  # python3
@@ -5,7 +11,24 @@ except ImportError:  # python3
 
 from . import rules
 
+def parse_date(nxlog):
+    """
+    :param str nxlog: A naxsi log ( https://github.com/nbs-system/naxsi/wiki/naxsilogs )
+    :return string: date string or empty string if we fail to find a date
+    """
+    date_regex = re.compile("""(((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)""" \
+                                """\s+([0-3]?[0-9])|([0-2]0[0-3][0-9](/|-)(0?[0-9]|1[0-2])""" \
+                                """(/|-)([0-3][0-9])))\s+[0-1][0-9|2[0-3]):[0-5][0-9]:[0-5][0-9]""" \
+                                """(\+0[0-9]|1[0-2])?""")
 
+    end=nxlog.find("[error]")
+    ret=""
+    if end > 0:
+        match=re.search(date_regex, nxlog[:end])
+        if match:
+            ret = match.group(0)
+    return ret
+        
 def parse_nxlog(nxlog):
     """
 
@@ -14,9 +37,9 @@ def parse_nxlog(nxlog):
     """
     errors = list()
     ret = dict()
-
     #re.match(r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2} \[error\]')
 
+    date = parse_date(nxlog)
     start = nxlog.find("ip=")
     if start < 0:
         errors.append('%s is an invalid extlog or nxlog, string "ip=" not found.' % nxlog)
@@ -38,6 +61,8 @@ def parse_nxlog(nxlog):
     for key, value in ret.items():
         ret[key] = value[0]
 
+    ret['date'] = unify_date(date)
+    ret['coords'] = coords(ip)
     return list(), ret
 
 
@@ -77,3 +102,75 @@ def explain_nxlog(nxlog):
 
     return explain
 
+def unify_date(date):
+    """ tries to parse a text date,
+    returns date object or None on error """
+    out_date_format = "%Y%m%dT%H:%M:%S"
+    idx = 0
+    res = ""
+    utc_shift=0
+    success = 0
+    supported_formats = [
+        "%b  %d %H:%M:%S",
+        "%b %d %H:%M:%S",
+        "%Y/%m/%d %H:%M:%S",
+        "%Y-%m-%d %H:%M:%S",
+        "%Y-%m-%dT%H:%M:%S"
+    #            "%Y-%m-%dT%H:%M:%S+%:z"
+        ]
+    # Seems coherent to store UTC time
+    # The RFC 3339 specifies CCYY-MM-DDThh:mm:ss[Z|(+|-)hh:mm] as a format
+    # This is not RFC 3339 compliant...
+    while date[idx] == " " or date[idx] == "\t":
+        idx += 1
+        success = 0
+    for date_format in supported_formats:
+        # strptime does not support numeric time zone, hack.
+        idx = date.find("+")
+        if idx != -1:
+            utc_shift = date[idx:]
+            date = date[:idx]
+        try:
+            x = datetime.strptime(date, date_format)
+            x = x.replace(hour=x.hour+int(utc_shift))
+            # ugly hack when we don't have year
+            if x.year==1900:
+                x=x.replace(year=datetime.now().year)
+            z = x.strftime(out_date_format)
+            success = 1
+            break
+        except:
+            #print "'"+clean_date+"' not in format '"+date_format+"'"
+            pass
+    if success == 0:
+        logging.critical("Unable to parse date format :'"+date+"'")
+        return ""
+    return z
+
+def coords(ip, db='/usr/share/GeoIP/GeoIPCity.dat'):
+    ret=None
+    try:
+        import GeoIP
+    except:
+        logging.warning("GeoIP python module not installed")
+        return ret
+    try:
+        socket.inet_aton(ip)
+        ip_type=4
+    except socket.error:
+        try:
+            socket.inet_pton(socket.AF_INET6, ip)
+            ip_type=6
+        except socket.error:
+            logging.warning("Ip %s is not parseable" % ip)
+            return None
+    r=None
+    if ip_type==4:
+        gi = GeoIP.open(db, GeoIP.GEOIP_STANDARD)
+        r = gi.record_by_addr(ip)
+    elif ip_type==6:
+        pass
+    # For the moment there's no useable ipv6 city database
+    if r is not None:
+        ret= "[%s, %s]" % (r['latitude'], r['longitude'])
+    return ret
--- tests/test_nxlog.py
+++ tests/test_nxlog.py
@@ -16,4 +16,46 @@ class TestProcessing(TestCase):
                                'total_blocked': '204', 'ip': 'X.X.X.X', 'server': 'Y.Y.Y.Y',
                                'learning': '0', 'zone0': 'HEADERS', 'score0': '8',
                                'var_name0': 'user-agent', 'cscore0': '$UWA', 'id0': '42000227',
-                               'total_processed': '472', 'vers': '0.52'})
+                               'total_processed': '472', 'vers': '0.52', 'date': '20131110T07:36:19'})
+
+    def test_parse_date(self):
+        _nxlog = list()
+        _nxlog.append('2013/11/10 07:36:19 [error] 8278#0: *5932 NAXSI_FMT: ip=X.X.X.X&server=Y.Y.Y.Y&')
+        _nxlog[0] += 'uri=/phpMyAdmin-2.8.2/scripts/setup.php&learning=0&vers=0.52&total_processed=472&total_blocked=204&'
+        _nxlog[0] += 'block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,'
+        _nxlog[0] += 'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
+        _nxlog.append('2013/3/10 14:36:19 [error] 8278#0: *5932 NAXSI_FMT: ip=X.X.X.X&server=Y.Y.Y.Y&')
+        _nxlog[1] += 'uri=/phpMyAdmin-2.8.2/scripts/setup.php&learning=0&vers=0.52&total_processed=472&total_blocked=204&'
+        _nxlog[1] += 'block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,'
+        _nxlog[1] += 'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
+        _nxlog.append('2013-3-10 07:36:19 [error] 8278#0: *5932 NAXSI_FMT: ip=X.X.X.X&server=Y.Y.Y.Y&')
+        _nxlog[2] += 'uri=/phpMyAdmin-2.8.2/scripts/setup.php&learning=0&vers=0.52&total_processed=472&total_blocked=204&'
+        _nxlog[2] += 'block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,'
+        _nxlog[2] += 'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
+        _nxlog.append('Feb   4 07:36:19 [error] 8278#0: *5932 NAXSI_FMT: ip=X.X.X.X&server=Y.Y.Y.Y&')
+        _nxlog[3] += 'uri=/phpMyAdmin-2.8.2/scripts/setup.php&learning=0&vers=0.52&total_processed=472&total_blocked=204&'
+        _nxlog[3] += 'block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,'
+        _nxlog[3] += 'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
+        _nxlog.append('2017-02-20T10:59:03+01:00 example.com nginx: Feb   4 07:36:19 [error] 8278#0: *5932 NAXSI_FMT: ip=X.X.X.X&server=Y.Y.Y.Y&')
+        _nxlog[4] += 'uri=/phpMyAdmin-2.8.2/scripts/setup.php&learning=0&vers=0.52&total_processed=472&total_blocked=204&'
+        _nxlog[4] += 'block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,'
+        _nxlog[4] += 'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
+        ret=list()
+        for _line in _nxlog:
+            ret.append(nxlog.parse_date(_line))
+        self.assertEqual(ret, ['2013/11/10 07:36:19', '2013/3/10 14:36:19', '2013-3-10 07:36:19', 'Feb   4 07:36:19', 'Feb   4 07:36:19']) 
+
+    def test_unify_date(self):
+        _dates=['2013/11/10 07:36:19', '2013/3/10 14:36:19', '2013-3-10 07:36:19', 'Jul 07   07:36:19', 'Feb  4 07:36:19']
+        ret=list()
+        for _date in _dates:
+            ret.append(nxlog.unify_date(_date))
+        self.assertEqual(ret, ['20131110T07:36:19', '20130310T14:36:19', '20130310T07:36:19', '20170707T07:36:19', '20170204T07:36:19'])
+        
+    def test_coords(self):
+        _ips=['198.41.0.4', '192.33.4.12', '192.5.5.241', '192.36.148.17', '2001:500:9f::42', '2001:500:84::b' ]
+        ret=list()
+        for _ip in _ips:
+            ret.append(nxlog.coords(_ip))
+        self.assertEqual(ret, ['[38.9880981445, -77.4755020142]', '[38.0, -97.0]', '[38.0, -97.0]', '[59.3293991089, 18.0685997009]'] )
+                    

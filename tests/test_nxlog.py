d4999168f077ea88a19fc11292664002159a6d5c
Manuel Sabban <manu@abban.eu>
2017-05-30 13:19:43 +0200

    Fix unittest regression.

--- tests/test_nxlog.py
+++ tests/test_nxlog.py
@@ -13,10 +13,10 @@ class TestProcessing(TestCase):
         _nxlog[0] += 'block=0&cscore0=$UWA&score0=8&zone0=HEADERS&id0=42000227&var_name0=user-agent, client: X.X.X.X,'
         _nxlog[0] += 'server: blog.memze.ro, request: "GET /phpMyAdmin-2.8.2/scripts/setup.php HTTP/1.1", host: "X.X.X.X"'
         _nxlog.append('2017-02-20T10:59:03+01:00 xxxx nginx: 2017/02/20 10:59:03 [error] 31557#0: *40698245 NAXSI_FMT:')
-        _nxlog[1] += 'ip=1.0.0.1&server=Y.Y.Y.Y&uri=/bidon&learning=1&vers=0.55.3&total_processed=11&total_blocked=11&'
+        _nxlog[1] += 'ip=X.X.X.X&server=Y.Y.Y.Y&uri=/bidon&learning=1&vers=0.55.3&total_processed=11&total_blocked=11&'
         _nxlog[1] += 'block=1&cscore0=$SQL&score0=40&cscore1=$XSS&score1=64&zone0=HEADERS&id0=1005&var_name0=cookie&'
         _nxlog[1] += 'zone1=HEADERS&id1=1010&var_name1=cookie&zone2=HEADERS&id2=1011&var_name2=cookie&zone3=HEADERS&'
-        _nxlog[1] += 'id3=1315&var_name3=cookie, client: 1.0.0.1, server: Y.Y.Y.Y, request: "GET /bidon HTTP/1.1", '
+        _nxlog[1] += 'id3=1315&var_name3=cookie, client: X.X.X.X, server: Y.Y.Y.Y, request: "GET /bidon HTTP/1.1", '
         _nxlog[1] += 'host: "Y.Y.Y.Y", referrer: "http://Y.Y.Y.X/"'
         t = list()
         for _line in _nxlog:
@@ -29,24 +29,24 @@ class TestProcessing(TestCase):
                                 'var_name': 'user-agent', 'cscore0': '$UWA', 'id': '42000227',
                                 'total_processed': '472', 'vers': '0.52', 'date': '20131110T07:36:19', 'coords': None},
                                 {'total_processed': '11', 'vers': '0.55.3', 'learning': '1', 'cscore0': '$SQL',
-                                'cscore1': '$XSS', 'ip': '1.0.0.1', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
+                                'cscore1': '$XSS', 'ip': 'X.X.X.X', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
                                 'var_name': 'cookie', 'score0': '40', 'score1': '64', 'total_blocked': '11',
-                                'date': '20170220T10:59:03', 'coords': '[-37.700000, 145.183300]',
+                                'date': '20170220T10:59:03', 'coords': None,
                                 'zone': 'HEADERS', 'id': '1005', 'block': '1'},
                                 {'total_processed': '11', 'vers': '0.55.3', 'learning': '1', 'cscore0': '$SQL',
-                                     'cscore1': '$XSS', 'ip': '1.0.0.1', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
+                                     'cscore1': '$XSS', 'ip': 'X.X.X.X', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
                                 'var_name': 'cookie', 'score0': '40', 'score1': '64', 'total_blocked': '11',
-                                'date': '20170220T10:59:03', 'coords': '[-37.700000, 145.183300]', 'zone': 'HEADERS',
+                                'date': '20170220T10:59:03', 'coords': None, 'zone': 'HEADERS',
                                 'id': '1010', 'block': '1'},
                                 {'total_processed': '11', 'vers': '0.55.3', 'learning': '1', 'cscore0': '$SQL',
-                                     'cscore1': '$XSS', 'ip': '1.0.0.1', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
+                                     'cscore1': '$XSS', 'ip': 'X.X.X.X', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
                                 'var_name': 'cookie', 'score0': '40', 'score1': '64', 'total_blocked': '11',
-                                'date': '20170220T10:59:03', 'coords': '[-37.700000, 145.183300]', 'zone': 'HEADERS',
+                                'date': '20170220T10:59:03', 'coords': None, 'zone': 'HEADERS',
                                 'id': '1011', 'block': '1'},
                                 {'total_processed': '11', 'vers': '0.55.3', 'learning': '1', 'cscore0': '$SQL',
-                                     'cscore1': '$XSS', 'ip': '1.0.0.1', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
+                                     'cscore1': '$XSS', 'ip': 'X.X.X.X', 'uri': '/bidon', 'server': 'Y.Y.Y.Y',
                                 'var_name': 'cookie', 'score0': '40', 'score1': '64', 'total_blocked': '11',
-                                'date': '20170220T10:59:03', 'coords': '[-37.700000, 145.183300]', 'zone': 'HEADERS',
+                                'date': '20170220T10:59:03', 'coords': None, 'zone': 'HEADERS',
                                 'id': '1315', 'block': '1'}])
 
     def test_parse_date(self):
